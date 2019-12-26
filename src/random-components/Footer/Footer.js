3e76260a7388397f3ac12b22500145bd370129da
Patrick Hanna <patrickjh1998@hotmail.com>
2019-12-25 20:55:41 -0500

    made it so that the links in the footer actually work

--- src/random-components/Footer/Footer.js
+++ src/random-components/Footer/Footer.js
@@ -4,11 +4,11 @@ import './Footer.scss';
 
 export default function Footer() {
 
-    function ContactMethod({ platform, info }) {
-        return <div className="ContactMethod">
+    function ContactMethod({ platform, info, link, shouldOpenInNewWindow = false}) {
+        return <a href={link} target={ shouldOpenInNewWindow ? "_blank" : ""} rel="noopener noreferrer" className="ContactMethod">
             <div className="platform">{platform}</div>
             <div className="info">{info}</div>
-        </div>
+        </a>
     }
 
 
@@ -20,14 +20,14 @@ export default function Footer() {
         </div>
         <div className="contact-method-boxes">
             {contactMethods.map((x, i) => {
-                return <ContactMethod key={i} platform={x.platform} info={x.info} />
+                return <ContactMethod key={i} platform={x.platform} info={x.info} link={x.link} shouldOpenInNewWindow={x.shouldOpenInNewWindow}/>
             })}
         </div>
     </div>
 }
 
 const contactMethods = [
-    { platform: "Facebook", info: "@Screws & Fasteners World" },
-    { platform: "Email", info: "info@screwsworldbahamas.com" },
-    { platform: "Phone", info: "(242) 326-1976" }
+    { platform: "Facebook", info: "@Screws & Fasteners World", link: "https://www.facebook.com/Screws-Fasteners-World-1472896606259468/", shouldOpenInNewWindow: true},
+    { platform: "Email", info: "info@screwsworldbahamas.com", link: "mailto:info@screwsworldbahamas.com"},
+    { platform: "Phone", info: "(242) 326-1976", link: "tel:242 326 1976"}
 ];
