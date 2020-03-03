f237d92a226d5c56d6ed6e7c680ea02c766bd3a6
Patrick Hanna <patrickjh1998@hotmail.com>
2020-03-03 08:58:21 -0500

    compressed asset images, fixed minor ui bugs

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,6 @@ import './index.scss';
 ReactDom.render(<App/>, document.getElementById('root'));
 
 
-console.warn("when selecting an image on ios, sometimes only half of the image shows up in the actual space for the image on the website.");
-console.warn("change the dashboard password to be something more complex");
-console.warn("png file isn't compressed properly on ios, file becomes larger instead of smaller");
 
-console.warn("compress asset images");
+console.warn("change the dashboard password to be something more complex");
+console.warn("fix products page issues in edge");
